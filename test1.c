28bbad285e35b7046917ccf7c18145a29467c0e5
Squashable <josh@hudgell.uk>
2026-03-30 14:02:33 +0100

    print off all statements (test 3)
    Everything has been put back into the main loop because I don't yet know how to take variables out of a nested loop.

--- test1.c
+++ test1.c
@@ -2,15 +2,30 @@
 #include <string.h> 
 #include <stdlib.h>
 
-int OpenCSV(void){
+int main(){
     FILE *fp = fopen("ReadFrom.csv", "r");
     if(fp == NULL) {
         printf("Error opening file\n");
         return 1;
     }
-}
+    char line[256];
+    fgets(line, sizeof(line), fp);
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        char *token;
+        token = strtok(line, ",");
+        float time = atof(token);
+
+        token = strtok(NULL, ",");
+        float temperature = atof(token);
+
+        token = strtok(NULL, ",");
+        float pressure = atof(token);
+
+        printf("Time =%.1f, temp = %.1f, Pressure = %.1f\n",
+            time, temperature, pressure);
 
 
-int main(){
-    OpenCSV();
+    }
+    fclose(fp);
+    return(0);
 }
